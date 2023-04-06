5afc3fe307366ed5193cf790f4108b462f16a176
ysyx_22040343-Chen Tingrun <chentingrun1011@163.com>
2023-04-06 21:20:27 +0800

    update

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,11 +2,11 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
+use std::vec;
 
 fn main() {
-    let a = ???
-
+    let a: Vec<i32> = Vec::with_capacity(100);
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
